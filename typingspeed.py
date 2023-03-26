0418126ce907ec57aa2ca86347891cc347386ff5
Gurminder singh <123150161+gurmindersingh5@users.noreply.github.com>
2023-03-26 03:48:00 -0400

    Update typingspeed.py

--- typingspeed.py
+++ typingspeed.py
@@ -2,12 +2,10 @@ import datetime
 
 sentence = "hello how are you"
 l1 = list(sentence.split(" "))
-print("word:", sentence)
+print("Word:", sentence)
 before = datetime.datetime.now()
-print(before)
 inp = input()
 after = datetime.datetime.now()
-print(after)
 if inp == sentence:
     print(f"{int(len(l1)/(after.second-before.second)*60)} words per minute")
 else:
